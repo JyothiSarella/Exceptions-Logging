c8425ea5fad4fc0306e2510fd8f249e2d85a2053
JyothiSarella <60711362+JyothiSarella@users.noreply.github.com>
2020-02-26 22:13:15 +0530

    Add files via upload

---
+++ HouseConstructionCost.java
@@ -0,0 +1,25 @@
+package Logging.epam;
+
+import java.util.*;
+public class HouseConstructionCost {
+
+public double estimateCost(String material, double area, boolean fullyAutomated) {
+	// TODO Auto-generated method stub
+	if(material.equals("standardMaterial") && fullyAutomated == false)
+	{
+	return 1200*area;
+	}
+	else if(material.equals("aboveStandardMaterial") && fullyAutomated == false)
+	{
+	return 1500*area;
+	}
+	else if(material.equals("highStandardMaterial") && fullyAutomated == false)
+	{
+	return 1800*area;
+	}
+	else
+	{
+	return 2500*area;
+	}
+}
+}
